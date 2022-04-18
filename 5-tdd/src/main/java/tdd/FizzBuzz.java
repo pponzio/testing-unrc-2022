86b7305617049a25fc08381fe0be7d0c3b458bce
Pablo Ponzio <pponzio@dc.exa.unrc.edu.ar>
2022-04-18 17:51:37 -0300

    Una solucion para el kata fizzbuzz

--- 5-tdd/src/main/java/tdd/FizzBuzz.java
+++ 5-tdd/src/main/java/tdd/FizzBuzz.java
@@ -1,16 +1,46 @@
 package tdd;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class FizzBuzz {
 
 	public static String fizzbuzz(int i) {
-		if (i % 3 == 0 && i % 5 == 0)
-			return "FizzBuzz";
-		if (i % 5 == 0)
-			return "Buzz";
-		if (i % 3 == 0)
-			return "Fizz";
+		String res = "";
+
+		if (i % 3 == 0 || isDigitOf(i, 3))
+			res += "Fizz";
+		if (i % 5 == 0 || isDigitOf(i, 5))
+			res += "Buzz";
+	
+		if (res.equals(""))
+			return new Integer(i).toString();
+		else
+			return res;
+	}
+
+	public static boolean isDigitOf(int i, int d) {
+		while (i > 0) {
+			if (i % 10 == d)
+				return true;
+			i = i / 10;
+		}
 		
-		return new Integer(i).toString();
+		return false;
+	}
+
+	public static List<String> listFizzBuzz(int i, int j) {
+		List<String> res = new LinkedList<>();
+		for (int k = i; k <= j; k++) 
+			res.add(fizzbuzz(k));
+		return res;
+	}
+	
+	public static void printListFizzBuzz(int i, int j) {
+		List<String> res = listFizzBuzz(i, j);
+		for (String e: res) {
+			System.out.println(e);
+		}
 	}
 
 }
--- 5-tdd/src/test/java/tdd/TestFizzBuzz.java
+++ 5-tdd/src/test/java/tdd/TestFizzBuzz.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 
 public class TestFizzBuzz {
 
@@ -19,12 +21,91 @@ public class TestFizzBuzz {
 		"3, Fizz",
 		"5, Buzz",
 		"10, Buzz",
+		"13, Fizz",
+		"31, Fizz",
+		"52, Buzz",
+		"51, FizzBuzz",
+		"35, FizzBuzz",
 	})
 	void fizzBuzzParamTest(int i, String expected) {
 		String res = FizzBuzz.fizzbuzz(i);
 		assertThat(res).isEqualTo(expected);
 	}
 	
+	@Test
+	void threeIsADigitOfThirtyOne() {
+		boolean res = FizzBuzz.isDigitOf(31, 3);
+		assertThat(res).isTrue();
+	}
+	
+	@Test
+	void threeIsADigitOfTwentyOne() {
+		boolean res = FizzBuzz.isDigitOf(21, 3);
+		assertThat(res).isFalse();
+	}
+	
+	@Test
+	void threeIsADigitOfIsTrue() {
+		boolean res = FizzBuzz.isDigitOf(310, 3);
+		assertThat(res).isTrue();
+	}
+	
+	@Test
+	void threeIsADigitOfThree() {
+		boolean res = FizzBuzz.isDigitOf(3, 3);
+		assertThat(res).isTrue();
+	}
+	
+	@Test
+	void sixIsNotADigitOfThree() {
+		boolean res = FizzBuzz.isDigitOf(6, 3);
+		assertThat(res).isFalse();
+	}
+	
+	@Test
+	void sevenIsNotADigitOfThree() {
+		boolean res = FizzBuzz.isDigitOf(7, 3);
+		assertThat(res).isFalse();
+	}
+	
+	@Test
+	void fiveIsADigitOfFive() {
+		boolean res = FizzBuzz.isDigitOf(5, 5);
+		assertThat(res).isTrue();
+	}
+	
+	@Test
+	void fiveIsNotADigitOfSeven() {
+		boolean res = FizzBuzz.isDigitOf(7, 5);
+		assertThat(res).isFalse();
+	}
+	
+	@Test
+	void fiveIsADigitOfFifteen() {
+		boolean res = FizzBuzz.isDigitOf(15, 5);
+		assertThat(res).isTrue();
+	}
+	
+	@Test
+	void ZeroIsADigitOfTeen() {
+		boolean res = FizzBuzz.isDigitOf(10, 0);
+		assertThat(res).isTrue();
+	}
+	
+	@Test
+	void listFizzBuzzOneToFive() {
+		List<String> res = FizzBuzz.listFizzBuzz(1, 5);
+		assertThat(res).containsExactly("1", "2", "Fizz", 
+				"4", "Buzz");
+	}
+	
+	
+	@Test
+	void listFizzBuzzOneToTen() {
+		List<String> res = FizzBuzz.listFizzBuzz(1, 10);
+		assertThat(res).containsExactly("1", "2", "Fizz", 
+				"4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz");
+	}
 	
 	
 }
